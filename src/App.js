ef5d70c637aa9f15f20dc9b55a7a2e79058dfae7
tej00001 <tejachintu207@gmail.com>
2023-03-21 10:18:43 +0530

    Adding Task21 Fixing the Cart problems

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
-import { uiActions } from "./store/ui-slice";
+import { Fragment, useEffect, useState } from "react";
+// import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-action";
 
 let isInitial = true;
 
@@ -17,52 +18,21 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
+    dispatch(fetchCartData());
+  }, [dispatch]);
+  // const responseData = await response.json();
 
-      const response = await fetch(
-        "https://shopping-app-with-reducer-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-      // const responseData = await response.json();
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
+  useEffect(() => {
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed",
-        })
-      );
-    });
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart]);
   return (
-    <>
+    <Fragment>
       {notification && (
         <Notification
           status={notification.status}
@@ -74,7 +44,7 @@ function App() {
         {showCart && <Cart />}
         <Products />
       </Layout>
-    </>
+    </Fragment>
   );
 }
 
